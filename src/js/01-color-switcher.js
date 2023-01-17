652edb75933ec273313c1a450adee425344cd48d
Agata Wójcik <agata_wojcik@onet.pl>
2023-01-17 18:22:56 +0100

    function getRandomHexColor

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,11 @@
 let btnStart = document.querySelector('button[data-start]');
 let btnStop = document.querySelector('button[data-stop]');
+let body = document.querySelector('body');
 
-btnStart.addEventListener('click', console.log('start'));
-btnStop.addEventListener('click', console.log('stop'));
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
-//test
+
+btnStart.addEventListener('click', start);
+btnStop.addEventListener('click', stop);
