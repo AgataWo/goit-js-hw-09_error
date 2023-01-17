52b6a4a8b90a9493bcdd1bd2c76bc2015ee1637c
Agata Wójcik <agata_wojcik@onet.pl>
2023-01-17 18:13:24 +0100

    btn test

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,5 @@
+const btnStart = document.querySelector('data-start');
+const btnStop = document.querySelector('data-stop');
 
+btnStart.addEventListener('click', console.log('start'));
+btnStop.addEventListener('click', console.log('stop'));
