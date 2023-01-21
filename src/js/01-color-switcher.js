ba18dc0cc9f24c451b55eb78fbf339fe8a3054f3
Agata Wójcik <agata_wojcik@onet.pl>
2023-01-21 17:23:58 +0100

    test

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,6 +6,23 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
 
+let timer = null;
+stopBtn.disabled = true;
+
+let start = () => {
+  let colorsBg = () => {
+    background.style.backgroundColor = `${getRandomHexColor()}`;
+  };
+  timer = setInterval(colorsBg, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+};
+
+let stop = () => {
+  clearInterval(timer);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+};
 
 btnStart.addEventListener('click', start);
 btnStop.addEventListener('click', stop);
