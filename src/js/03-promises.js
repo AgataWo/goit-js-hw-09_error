f48f88e5ff3dc87af3c7d737d757083ce40ffa36
Agata Wójcik <agata_wojcik@onet.pl>
2023-01-22 14:03:35 +0100

    promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+let inputDelay = document.querySelector('input[name="delay"]');
+let inputStep = document.querySelector('input[name="step"]');
+let inputAmount = document.querySelector('input[name="amount"]');
+let submit = document.querySelector('form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+let start = e => {
+  e.preventDefault();
+
+  let valueDelay = Number(inputDelay.value);
+  let valueStep = Number(inputStep.value);
+  let valueAmount = Number(inputAmount.value);
+
+  for (let i = 0; i < valueAmount; i++) {
+    createPromise(i + 1, valueDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    valueDelay += valueStep;
+  }
+};
+submit.addEventListener('click', start);
